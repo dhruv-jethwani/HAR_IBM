3dd7713f4593594c6cfb576e6ca13480876277f1
Dhruv Jethwani <dhruvjethwani56@gmail.com>
2026-04-03 20:48:55 +0530

    Add Render API for Health Status

--- backend/app.py
+++ backend/app.py
@@ -255,6 +255,10 @@ def upload_image():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/healthz')
+def health_check():
+    return "OK", 200
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
